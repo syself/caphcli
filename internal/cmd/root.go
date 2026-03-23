4845de37a4ed487896545654321da8e4f6fece44
Thomas Güttler <thomas.guettler@syself.com>
2026-03-23 17:52:19 +0100

    :seedling: Initial commit
    ❯ go run .
    CAPH developer and operations CLI
    
    Usage:
      caphcli [command]
    
    Available Commands:
      check-bm-servers Validate rescue and provisioning reliability for one bare-metal server
      completion       Generate the autocompletion script for the specified shell
      help             Help about any command
    
    Flags:
      -h, --help   help for caphcli
    
    Use "caphcli [command] --help" for more information about a command.

---
+++ internal/cmd/root.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"github.com/go-logr/logr"
+	"github.com/spf13/cobra"
+	ctrlLog "sigs.k8s.io/controller-runtime/pkg/log"
+
+	"github.com/syself/caphcli/internal/data"
+)
+
+func NewRootCommand() *cobra.Command {
+	ctrlLog.SetLogger(logr.Discard())
+	data.RegisterEmbeddedInstallImageTGZ()
+
+	rootCmd := &cobra.Command{
+		Use:           "caphcli",
+		Short:         "CAPH developer and operations CLI",
+		SilenceUsage:  true,
+		SilenceErrors: false,
+	}
+
+	rootCmd.AddCommand(newCheckBMServersCommand())
+
+	return rootCmd
+}
